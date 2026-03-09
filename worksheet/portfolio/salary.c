7ec345c65c1b8b42521a9c387ad3e6b0bb53692c
fizeenf <lhxz0885@leeds.ac.uk>
2026-03-09 14:52:06 +0000

    wk 2.3

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -15,21 +15,23 @@
     double tax_rate = 0.15;
 
     // calculate the deductions and final take-home salary
-    double ni_contribution = salary * ni_rate;
+    double ni_contributions = salary * ni_rate;
+    double salary_after_NI = salary - ni_contributions; 
 
     double taxable_amount = 0.0;
     if (salary_after_NI > 12500.0 ) {
       taxable_amount = salary_after_NI  - 12500.0;
     }
 
-    double tax_contribution = taxable_amount * tax_rate;
-    double take_home = salary - ni_contribution - tax_contribution;
+    double tax_contributions = taxable_amount * tax_rate;
+    double take_home = salary - ni_contributions - tax_contributions;
     // Use only these print statement with appropriate formatting and variable names
     //printf("Salary £...",var_name);
-    printf("Salary £% .2f/n", salary);
-    printf("NI contribution £% .2f/n", ni_contribution);
-    printf("Tax contribution £% .2f/n", tax_contribution);
-    printf("Take home salary £% .2f/n", take_home);
+
+    printf("Salary £% .2f\n", salary);
+    printf("NI contribution £% .2f\n", ni_contributions);
+    printf("Tax contribution £% .2f\n", tax_contributions);
+    printf("Take home salary £% .2f\n", take_home);
 
     return 0;
  }
